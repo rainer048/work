7c8b5bb7e178544d59ce0e54cc5db9f0175dc1c2
Dmitriy <31509173+godobrf@users.noreply.github.com>
2018-04-13 12:05:53 +0300

    Update 01_WhiteList.sol

--- 01/01_WhiteList.sol
+++ 01/01_WhiteList.sol
@@ -3,39 +3,32 @@ pragma solidity 0.4.21;
 
 contract CrowdsaleWithWhiteList is Crowdsale, Ownable {
 	
-	mapping(address => bool) users;
+    mapping(address => bool) users;
 	
-	modifier isInWhiteList(address user) {
-		require(users[user]);
-		_;
-	}
+    modifier isInWhiteList(address user) {
+	require(users[user]);
+	_;
+    }
     
     function addToWhiteList(address user) public external onlyOwner {
-		require(user != address(0));
-		users[user] = true;
-	}
+        require(user != address(0));
+	users[user] = true;
+    }
 	
-	function addManyToWhiteList(address[] user) public external onlyOwner {
-		for (int i = 0; i < user.length; i++) {
-			require(user[i] != address(0));
-		}
-		for (int i = 0; i < user.length; i++) {
-			users[user[i]] = true;
-		}
+    function addManyToWhiteList(address[] user) public external onlyOwner {
+	for (int i = 0; i < user.length; i++) {
+	    addToWhiteList(user[i]);
 	}
+    }
+
+    function removeFromWhiteList(address user) public external onlyOwner {
+        require(user != address(0));
+	users[user] = false;
+    }
 	
-	function removeFromWhiteList(address user) public external onlyOwner {
-		require(user != address(0));
-		users[user] = false;
+    function removeManyFromWhiteList(address[] user) public external onlyOwner {
+	for (int i = 0; i < user.length; i++) {
+	    removeFromWhiteList(user[i]);
 	}
-	
-	function removeManyFromWhiteList(address[] user) public external onlyOwner {
-		for (int i = 0; i < user.length; i++) {
-			require(user[i] != address(0));
-		}
-		for (int i = 0; i < user.length; i++) {
-			users[user[i]] = false;
-		}
-	}
-		
+    }
 }
